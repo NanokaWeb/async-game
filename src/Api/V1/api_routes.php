c3ca5ad7d8268859ac429d43239e0e07c794965f
Monnot Stéphane <monnot.stephane@gmail.com>
2016-02-16 14:05:03 +0100

    Add new routes for api

--- src/Api/V1/api_routes.php
+++ src/Api/V1/api_routes.php
@@ -3,20 +3,29 @@
 $api = app('Dingo\Api\Routing\Router');
 
 // Authentification routes
-$api->version('v1', ['middleware' => ['api.throttle', 'cors'], 'limit' => 100, 'expires' => 5], function ($api) {
-    // Auth email / password
-    $api->post('v1/auth/signup', 'NanokaWeb\AsyncGame\Api\V1\Controllers\AuthController@signup');
-    $api->post('v1/auth/login', 'NanokaWeb\AsyncGame\Api\V1\Controllers\AuthController@login');
+$api->version(
+    'v1',
+    [
+        'namespace'  => 'NanokaWeb\AsyncGame\Api\V1\Controllers',
+        'middleware' => ['api.throttle', 'cors'],
+        'limit' => 100,
+        'expires' => 5
+    ],
+    function ($api) {
+        // Auth email / password
+        $api->post('v1/auth/signup', 'AuthController@signup');
+        $api->post('v1/auth/login', 'AuthController@login');
 
-    // Auth Facebook token
-    $api->post('v1/auth/fblogin', 'NanokaWeb\AsyncGame\Api\V1\Controllers\AuthController@fbLogin');
+        // Auth Facebook token
+        $api->post('v1/auth/fblogin', 'AuthController@fbLogin');
 
-    // Auth Device
-    $api->post('v1/auth/devicelogin', 'NanokaWeb\AsyncGame\Api\V1\Controllers\AuthController@deviceLogin');
+        // Auth Device
+        $api->post('v1/auth/devicelogin', 'AuthController@deviceLogin');
 
 //    $api->post('v1/auth/recovery', 'NanokaWeb\AsyncGame\Api\V1\Controllers\AuthController@recovery');
 //    $api->post('v1/auth/reset', 'NanokaWeb\AsyncGame\Api\V1\Controllers\AuthController@reset');
-});
+    }
+);
 
 // Need auth
 $api->version('v1', ['middleware' => ['api.auth', 'api.throttle', 'cors'], 'limit' => 100, 'expires' => 5], function ($api) {
@@ -26,17 +35,28 @@ $api->version('v1', ['middleware' => ['api.auth', 'api.throttle', 'cors'], 'limi
         'middleware' => ['asyncgame.roles'],
         'roles'      => ['root', 'administrator', 'user'],
     ], function ($api) {
-        // Tests
-        $api->get('v1/me', 'MeController@show')
-            ->where('id', '[0-9]+')
-        ;
-        $api->get('v1/me/opponents/{nb}', 'MeController@opponents')
-            ->where('id', '[0-9]+')
+        /*
+         * Endpoint me
+         */
+        $api->get('v1/me', 'MeController@showMe');
+        $api->get('v1/me/opponents/{nb}', 'MeController@opponentsMe')
             ->where('nb', '[0-9]')
         ;
-        $api->put('v1/me', 'MeController@update');
+        $api->put('v1/me', 'MeController@updateMe');
 
-        $api->post('v1/me/coins', 'MeController@updateCoins');
+        $api->post('v1/me/coins', 'MeController@updateCoinsMe');
+
+        /*
+         * Endpoint seeds
+         */
+        $api->get('v1/seeds/search', 'SeedController@search');
+
+        $api->get('v1/seeds/{id}/games', 'SeedGameController@index')
+            ->where('id', '[0-9]+');
+
+        $api->post('v1/seeds/{id}/games', 'SeedGameController@store')
+            ->where('id', '[0-9]+');
+
     });
 
     $api->group([
